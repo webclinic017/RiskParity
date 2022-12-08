8ba468f3edf76e57d0ea289aea74cfaa38be3153
ra6it <kitmurraypyle@gmail.com>
2022-12-08 22:42:55 +1300

    more changes

--- Strategy_BackTest/Strategy.py
+++ Strategy_BackTest/Strategy.py
@@ -21,8 +21,8 @@ Hist = True # Use historical scenarios for risk measures that depend on scenario
 Rf = 0.04 # Risk free rate
 L = 1 # Risk aversion factor, only useful when obj is 'Utility'
 Points = 50 # Number of points of the frontier
-Start = '2019-10-01'
-End = '2019-11-01'
+Start = '2016-01-01'
+End = '2020-12-31'
 
 
 warnings.filterwarnings("ignore")
@@ -46,18 +46,16 @@ def excel_download():
     constraint_url = requests.get(constraint_url).content
     constraints = pd.read_excel(holdings_url,'Constraints',usecols="B:K", engine='openpyxl')
     constraints=pd.DataFrame(constraints)
-    print(constraints)
     prices = prices = yf.download(asset, start=Start, end=End)
     return asset_classes, constraints, prices, asset
         
 # Downloading data
 def data_download(asset_classes):
-    print("Downloading data...")
     asset = asset_classes['Asset'].to_string(index=False)
     data = yf.download(asset, start = Start, end = End)
     data = data.loc[:,('Adj Close', slice(None))]
-    Y = data.pct_change().dropna()
-    return Y
+    returns = data.pct_change().dropna()
+    return returns
     
 # Select method and estimate input parameters:
 def method():
@@ -65,8 +63,8 @@ def method():
     method_cov='hist' # Method to estimate covariance matrix based on historical data.
     return method_mu, method_cov
 
-def portfolio_object(assets,method_mu, method_cov):
-    Port = rp.Portfolio(returns=data_download(assets))
+def portfolio_object(assets,method_mu, method_cov,returns):
+    Port = rp.Portfolio(returns)
     Port.assets_stats(method_mu=method_mu, method_cov=method_cov, d=0.94)
     return Port
 
@@ -86,13 +84,11 @@ def ainequality(A,B,Port):
     Port.ainequality = A
     Port.binequality = B
     w = Port.optimization(model=Model, rm=Rm, obj=Obj, rf=Rf, l=L, hist=Hist)
-    print("Expected returns for", Start, "-", End,":",Port.mu)
-    print("Holdings for", Start, "-", End,":", w.T)
     frontier_create(Port,w)
+    return(w)
 
 def frontier_create(Port,w):
     frontier = Port.efficient_frontier(model=Model, rm=Rm, points=Points, rf=Rf, hist=Hist)
-    print(frontier.T.head())
     label = 'Max Risk Adjusted Return Portfolio' # Title of point
     mu = Port.mu # Expected returns
     cov = Port.cov # Covariance matrix
@@ -101,35 +97,18 @@ def frontier_create(Port,w):
                       rf=Rf, alpha=0.05, cmap='viridis', w=w, label=label,
                       marker='*', s=16, c='r', height=6, width=10, ax=None)
 
-def runner():
-    asset_classes, constraints, prices, asset = excel_download()
+def runner(asset_classes, constraints, prices, asset,returns):
     method_mu, method_cov = method()
-    Port = portfolio_object(asset_classes,method_mu, method_cov)
+    Port = portfolio_object(asset_classes,method_mu, method_cov, returns)
     A,B = constraints_weightings(constraints,asset_classes)
-    ainequality(A,B,Port)
-    returns(prices, asset_classes)
-
-def returns(prices, asset_classes):
-    pd.options.display.float_format = '{:.4%}'.format
-    data = prices.loc[:, ('Adj Close', slice(None))]
-    data.columns = asset_classes
-    returner = data.pct_change().dropna()
-    print(returner.head())
-    rebalance_dates(returner)
-
-def rebalance_dates(returner):
-    index = returner.groupby([returner.index.year, returner.index.month]).tail(1).index
-    index_2 = returner.index
-
-    # Quarterly Dates
-    index = [x for x in index if float(x.month) % 3.0 == 0 ] 
-
-    # Dates where the strategy will be backtested
-    index_ = [index_2.get_loc(x) for x in index if index_2.get_loc(x) > 1000]
-
-runner()
+    w = ainequality(A,B,Port)
+    #returns(prices, asset_classes)
+    return(Port, w)
 
 asset_classes, constraints, prices, asset = excel_download()
+
+
+
 
 
 
