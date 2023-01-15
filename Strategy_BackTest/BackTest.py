4acc7209cb1f13201a54e34b99a4ea29297c9869
ra6it <kitmurraypyle@gmail.com>
2023-01-15 19:57:08 +1300

    more adjustments

--- Strategy_BackTest/BackTest.py
+++ Strategy_BackTest/BackTest.py
@@ -4,6 +4,7 @@ import yfinance as yf
 import backtrader as bt
 import numpy as np 
 import warnings
+import riskfolio as rp
 import requests
 import plotly.graph_objects as go
 from calendar import monthrange
@@ -12,38 +13,28 @@ from dateutil.relativedelta import relativedelta
 warnings.filterwarnings("ignore")
 
 # Date range
-Start = '2018-01-01'
+Start = '2022-01-01'
 End = '2022-06-30'
 start = Start
 end = End
 
 date1 = datetime.datetime.strptime(Start, "%Y-%m-%d")
 date2 = datetime.datetime.strptime(End, "%Y-%m-%d")
-print(date1,date2)
 diff = relativedelta(date2, date1)
-print(diff.years, diff.months)
 
 months_between = (diff.years)*12 + diff.months + 1
+print(months_between)
 # Tickers of assets
 
 Model='Classic' # Could be Classic (historical), BL (Black Litterman) or FM (Factor Model)
 Rm = 'MV' # Risk measure used, this time will be variance
-Obj = 'MaxRet' # Objective function, could be MinRisk, MaxRet, Utility or Sharpe
+Obj = 'Sharpe' # Objective function, could be MinRisk, MaxRet, Utility or Sharpe
 Hist = True # Use historical scenarios for risk measures that depend on scenarios
 Rf = 0.04 # Risk free rate
 L = 1 # Risk aversion factor, only useful when obj is 'Utility'
 Points = 50 # Number of points of the frontier
 method_mu ='hist' # Method to estimate expected returns based on historical data.
 method_cov ='hist' # Method to estimate covariance matrix based on historical data.
-
-def constraints_weightings(constraints,asset_classes):
-    asset_classes = pd.DataFrame(asset_classes)
-    constraints = pd.DataFrame(constraints)
-    data = constraints.fillna("")
-    data = data.values.tolist()
-    A, B = rp.assets_constraints(constraints, asset_classes)
-    print(A,B)
-    return A, B
 
 def excel_download():
     holdings_url = "https://github.com/ra6it/RiskParity/blob/main/RiskParity_Holdings_Constraints.xlsx?raw=true"
@@ -56,43 +47,9 @@ def excel_download():
     asset_classes = asset_classes.sort_values(by=['Asset'])
     asset = assets['Asset'].values.tolist()
     asset = [x for x in asset if str(x) != 'nan']
-    constraint_url = "https://github.com/ra6it/RiskParity/blob/main/RiskParity_Holdings_Constraints.xlsx?raw=true"
-    constraint_url = requests.get(constraint_url).content
-    constraints = pd.read_excel(holdings_url,'Constraints',usecols="B:K", engine='openpyxl')
-    constraints=pd.DataFrame(constraints)
-    return asset_classes, constraints, asset
+    return asset_classes, asset
 
-def runner(asset_classes, constraints, returns):
-    method_mu = method_cov = 'hist'
-    Port = portfolio_object(asset_classes,method_mu, method_cov, returns)
-    A,B = constraints_weightings(constraints,asset_classes)
-    w, returns = ainequality(A,B,Port)
-    return(Port, w, returns)
-
-def portfolio_object(assets,method_mu, method_cov,returns):
-    Port = rp.Portfolio(returns)
-    Port.assets_stats(method_mu=method_mu, method_cov=method_cov, d=0.94)
-    return Port
-
-def ainequality(A,B,Port):
-    Port.ainequality = A
-    Port.binequality = B
-    w = Port.optimization(model=Model, rm=Rm, obj=Obj, rf=Rf, l=L, hist=Hist)
-    returns = frontier_create(Port,w)
-    return(w, returns)
-
-def frontier_create(Port,w):
-    frontier = Port.efficient_frontier(model=Model, rm=Rm, points=Points, rf=Rf, hist=Hist)
-    label = 'Max Risk Adjusted Return Portfolio' # Title of point
-    mu = Port.mu # Expected returns
-    cov = Port.cov # Covariance matrix
-    returns = Port.returns # Returns of the assets
-    #ax = rp.plot_frontier(w_frontier=frontier, mu=mu, cov=cov, returns=returns, rm=Rm,
-                      #rf=Rf, alpha=0.05, cmap='viridis', w=w, label=label,
-                      #marker='*', s=16, c='r', height=6, width=10, ax=None)
-    return(returns)
-
-asset_classes, constraints, asset = excel_download()
+asset_classes, asset = excel_download()
 
 assets = asset
 # Downloading data
@@ -108,11 +65,6 @@ new_df.columns = assets
 print(new_df)
 
 prices = new_df
-prices = prices.dropna()
-
-valid_assets = asset_classes['Asset'].isin(asset)
-
-asset_classes = asset_classes[valid_assets]
 
 ############################################################
 # Calculate assets returns
@@ -122,25 +74,14 @@ asset_classes = asset_classes[valid_assets]
 
 data = prices
 
-returns = data.pct_change().dropna()
+returns = data.pct_change()
 
-############################################################
-# Building Constraints
-############################################################
+valid_assets = asset_classes['Asset'].isin(asset)
+
+asset_classes = asset_classes[valid_assets]
 
 asset_classes = pd.DataFrame(asset_classes)
 asset_classes = asset_classes.sort_values(by=['Asset'])
-
-
-constraints = pd.DataFrame(constraints)
-
-############################################################
-# Building constraint matrixes for Riskfolio Lib
-############################################################
-
-import riskfolio as rp
-
-A,B = constraints_weightings(constraints,asset_classes)
 
 ############################################################
 # Building a loop that estimate optimal portfolios on
@@ -150,7 +91,6 @@ A,B = constraints_weightings(constraints,asset_classes)
 rms = ['MV']
 
 rng_start = pd.date_range(start, periods=months_between, freq='MS')
-print(rng_start)
 ret = returns
 
 ############################################################
@@ -166,10 +106,15 @@ for i in rng_start:
     rng_end = pd.date_range(i, periods=1, freq='M')
     for b in rng_end:
         Y = ret[i:b]
-        #Port, w, returns = runner(asset_classes, constraints, Y)
+        Y = Y.dropna()
+        print(Y)
         port = rp.Portfolio(returns = Y)
+        print(port)
         port.assets_stats(method_mu=method_mu, method_cov=method_cov, d=0.94)
         w = port.optimization(model=Model, rm=Rm, obj=Obj, rf=Rf, l=L, hist=Hist)
+        print(w)
+        #Convert the returns and weightings to numpy.
+
         re = returns.to_numpy()
         we = w.to_numpy()
         myreturns = re.T * we
