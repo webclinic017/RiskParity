7fa17abe4352319f840ce97527841c903ca3d1cc
ra6it <kitmurraypyle@gmail.com>
2023-03-02 20:38:39 +1300

    setup for shorting

--- Strategy_BackTest/Trend_Following.py
+++ Strategy_BackTest/Trend_Following.py
@@ -31,20 +31,21 @@ def calculate_rolling_average(ret, window):
 # To do this, I think for each day, we can have a dummy, 1 for above sma and 2 for below sma.
 
 def dummy_sma(rolling_df, ret):
-    dummy_df = pd.DataFrame(index=rolling_df.index)
-    dummy_short_df = pd.DataFrame(index=rolling_df.index)
+    dummy_L_df = pd.DataFrame(index=rolling_df.index)
+    dummy_LS_df = pd.DataFrame(index=rolling_df.index)
     for asset_name in rolling_df.columns:
     # Skip non-numeric columns
         if not np.issubdtype(rolling_df[asset_name].dtype, np.number):
             continue
             
         # Compare the prices of the asset for each date
-        dummy_df[asset_name] = (rolling_df[asset_name] < ret[asset_name]).astype(int)
-        dummy_short_df[asset_name] = np.where(rolling_df[asset_name] < ret[asset_name], 1, -1)
-    df_monthly = dummy_df.resample('M').mean()
-    return df_monthly, dummy_short_df
+        dummy_L_df[asset_name] = (rolling_df[asset_name] < ret[asset_name]).astype(int)
+        dummy_LS_df[asset_name] = np.where(rolling_df[asset_name] < ret[asset_name], 1, -1)
+    dummy_L_df  = dummy_L_df.resample('M').mean()
+    dummy_LS_df = dummy_LS_df.resample('M').mean()
+    return dummy_L_df, dummy_LS_df
 
 rolling_df = calculate_rolling_average(ret, min(200, len(ret)))
-df_monthly, dummy_short_df = dummy_sma(rolling_df, ret)
+dummy_L_df, dummy_LS_df = dummy_sma(rolling_df, ret)
  
 # Now, if the row for a specific contract is <0, then we can exclude it from our sample set, and it is not needed. This is part of the asset selection component.
