012b2e7bbaf96af5521b2ac3d662cc78b50f1f2f
ra6it <kitmurraypyle@gmail.com>
2023-03-06 17:40:26 +1300

    solved... I think

--- Strategy_BackTest/datamanagement.py
+++ Strategy_BackTest/datamanagement.py
@@ -30,10 +30,10 @@ def datamanagement_1(start, end):
             asset_name = ticker
         else:
             asset_name = ticker.info.get('longName', ticker.info.get('shortName', asset))
-
+        
+        df_list.append(pd.DataFrame(asset_2))
         print(asset, asset_name)
         asset_2 = pd.DataFrame(asset_2)
-        df_list = pd.concat(asset_2, axis=1)
     prices = pd.concat(df_list, axis=1)
     prices.columns = asset
     print(asset_name)
