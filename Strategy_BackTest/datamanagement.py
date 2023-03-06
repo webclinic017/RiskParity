ac716eaeee77977d9bb9f6a231360ba77a8907ce
ra6it <kitmurraypyle@gmail.com>
2023-03-06 17:28:34 +1300

    ghance

--- Strategy_BackTest/datamanagement.py
+++ Strategy_BackTest/datamanagement.py
@@ -24,7 +24,7 @@ def datamanagement_1(start, end):
     print(asset)
     for i in asset:
         asset_2 = yf.download(i, start=start, end=end)['Adj Close']
-        asset_name = yf.download(i, start=start, end=end).get_full_name()
+        asset_name = yf.Ticker(i).info['longName']
         df_list.append(pd.DataFrame(asset_2))
     prices = pd.concat(df_list, axis=1)
     prices.columns = asset
