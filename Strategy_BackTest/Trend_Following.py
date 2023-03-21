ce1c593afd44f0d7d1168d424fdb4cfbb97fad62
ra6it <kitmurraypyle@gmail.com>
2023-03-21 20:25:22 +1300

    fix

--- Strategy_BackTest/Trend_Following.py
+++ Strategy_BackTest/Trend_Following.py
@@ -5,7 +5,7 @@ import numpy as np
 import math
 from datetime import date
 import matplotlib.pyplot as plt
-Start = '2022-01-01'
+Start = '2019-01-01'
 End = date.today().strftime("%Y-%m-%d")
 number_of_iter = 50
 
