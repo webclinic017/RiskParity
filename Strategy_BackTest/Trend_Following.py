6d8f3301b1a60140189b9f91354bd9dbeb11741c
ra6it <kitmurraypyle@gmail.com>
2023-04-05 16:05:05 +1200

    dadasad

--- Strategy_BackTest/Trend_Following.py
+++ Strategy_BackTest/Trend_Following.py
@@ -4,7 +4,7 @@ from datamanagement import *
 import numpy as np
 from datetime import date
 import matplotlib.pyplot as plt
-Start = '2022-01-01'
+Start = '2021-12-01'
 End = '2023-02-28' # date.today().strftime("%Y-%m-%d")
 number_of_iter = 1000
 long    = 200
